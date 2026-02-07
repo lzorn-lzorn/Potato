900b438720d65db6d2ed3e3d382cfbcb672ac740
lizhuoran <liZhuoran1998@outlook.com>
2026-02-07 18:12:45 +0800

    Potato Restart

--- Editor/Main.cpp
+++ Editor/Main.cpp
@@ -1,9 +1,142 @@
 #include <iostream>
+#include <print>
+
+#define SDL_MAIN_USE_CALLBACKS  
+#include <SDL3/SDL.h>
+#include <SDL3/SDL_main.h>
 
 #include "Runtime.hpp"
 
-int main()
-{
-	std::cout << GetGreeting() << std::endl;
-	return 0;
+
+// int main(int argc, char *argv[])
+// {
+// 	std::cout << GetGreeting() << std::endl;
+// 	return 0;
+// }
+
+#include <cmath>
+#include <string_view>
+#include <filesystem>
+#include <thread>
+constexpr uint32_t windowStartWidth = 400;
+constexpr uint32_t windowStartHeight = 400;
+
+struct AppContext {
+    SDL_Window* window;
+    SDL_Renderer* renderer;
+    SDL_Texture* messageTex, *imageTex;
+    SDL_FRect messageDest;
+    SDL_AppResult app_quit = SDL_APP_CONTINUE;
+};
+
+SDL_AppResult SDL_Fail(){
+    SDL_LogError(SDL_LOG_CATEGORY_CUSTOM, "Error %s", SDL_GetError());
+    return SDL_APP_FAILURE;
+}
+
+SDL_AppResult SDL_AppInit(void** appstate, int argc, char* argv[]) {
+	std::println("SDL_AppInit");
+    // init the library, here we make a window so we only need the Video capabilities.
+    if (not SDL_Init(SDL_INIT_VIDEO | SDL_INIT_AUDIO)){
+        return SDL_Fail();
+    }
+    
+    
+    // create a window
+   
+    SDL_Window* window = SDL_CreateWindow("SDL Minimal Sample", windowStartWidth, windowStartHeight, SDL_WINDOW_RESIZABLE | SDL_WINDOW_HIGH_PIXEL_DENSITY);
+    if (not window){
+        return SDL_Fail();
+    }
+    
+    // create a renderer
+    SDL_Renderer* renderer = SDL_CreateRenderer(window, NULL);
+    if (not renderer){
+        return SDL_Fail();
+    }
+    
+    // load the font
+#if __ANDROID__
+    std::filesystem::path basePath = "";   // on Android we do not want to use basepath. Instead, assets are available at the root directory.
+#else
+    auto basePathPtr = SDL_GetBasePath();
+     if (not basePathPtr){
+        return SDL_Fail();
+    }
+     const std::filesystem::path basePath = basePathPtr;
+#endif
+;
+
+    
+    // print some information about the window
+    SDL_ShowWindow(window);
+    {
+        int width, height, bbwidth, bbheight;
+        SDL_GetWindowSize(window, &width, &height);
+        SDL_GetWindowSizeInPixels(window, &bbwidth, &bbheight);
+        SDL_Log("Window size: %ix%i", width, height);
+        SDL_Log("Backbuffer size: %ix%i", bbwidth, bbheight);
+        if (width != bbwidth){
+            SDL_Log("This is a highdpi environment.");
+        }
+    }
+
+    // set up the application data
+    *appstate = new AppContext{
+       .window = window,
+       .renderer = renderer,
+    };
+    
+    SDL_SetRenderVSync(renderer, -1);   // enable vysnc
+    
+    SDL_Log("Application started successfully!");
+
+    return SDL_APP_CONTINUE;
+}
+
+SDL_AppResult SDL_AppEvent(void *appstate, SDL_Event* event) {
+	std::println("SDL_AppEvent: {:04x}", event->type);
+    auto* app = (AppContext*)appstate;
+    
+    if (event->type == SDL_EVENT_QUIT) {
+        app->app_quit = SDL_APP_SUCCESS;
+    }
+
+    return SDL_APP_CONTINUE;
+}
+
+SDL_AppResult SDL_AppIterate(void *appstate) {
+	// std::println("SDL_AppIterate");
+    auto* app = (AppContext*)appstate;
+
+    // draw a color
+    auto time = SDL_GetTicks() / 1000.f;
+    auto red = (std::sin(time) + 1) / 2.0 * 255;
+    auto green = (std::sin(time / 2) + 1) / 2.0 * 255;
+    auto blue = (std::sin(time) * 2 + 1) / 2.0 * 255;
+    
+    SDL_SetRenderDrawColor(app->renderer, red, green, blue, SDL_ALPHA_OPAQUE);
+    SDL_RenderClear(app->renderer);
+
+    // Renderer uses the painter's algorithm to make the text appear above the image, we must render the image first.
+    SDL_RenderTexture(app->renderer, app->imageTex, NULL, NULL);
+    SDL_RenderTexture(app->renderer, app->messageTex, NULL, &app->messageDest);
+
+    SDL_RenderPresent(app->renderer);
+
+    return app->app_quit;
+}
+
+void SDL_AppQuit(void* appstate, SDL_AppResult result) {
+	std::println("SDL_AppQuit");
+    auto* app = (AppContext*)appstate;
+    if (app) {
+        SDL_DestroyRenderer(app->renderer);
+        SDL_DestroyWindow(app->window);
+        
+        delete app;
+    }
+
+    SDL_Log("Application quit successfully!");
+    SDL_Quit();
 }
